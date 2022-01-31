2b5d2cc26d6b780e293b1076117afdd9ee8e1fbc
Matthias Hertel <mathertel@hotmail.com>
2022-01-31 20:12:48 +0100

    index page reduced

---
+++ pages.js
@@ -0,0 +1,25 @@
+// pages.js
+
+// lazy load any htm content
+function lazyLoadHTM(q, url) {
+  fetch(url, { overrideMimeType: 'application/text' })
+    .then(res => res.text())
+    .then(txt => {
+      document.querySelector(q).appendChild(document.createRange().createContextualFragment(txt));
+    });
+}
+
+function lazyLoadMD(q, url) {
+  fetch(url, { overrideMimeType: 'application/text' })
+    .then(res => res.text())
+    .then(txt => {
+      var hText = md.render(txt);
+      hText = hText.replace(/href="([\w\-\/]+).md"/g, 'href="#page=$1.md"');
+      hText = hText.replace(/href="([\w\-\/]+)"/g, 'href="#page=$1.md"');
+      document.querySelector(q).innerHTML = hText;
+    })
+    .catch(err => {
+      console.err('cannot lazyLoadMD', url, err);
+    });
+}
+
