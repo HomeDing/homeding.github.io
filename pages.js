cdb994310cb1dffce9180f7fb423f8769af11520
mathertel <mathertel@hotmail.com>
2022-05-04 16:20:18 +0000

    Deploying to gh-pages from @ HomeDing/homeding.github.io@8d334e205e3a2f469fe60057dc3a52471914dfc9 🚀

--- pages.js
+++ pages.js
@@ -1,32 +1,12 @@
 // pages.js
 
-// simplified YAML parser supporting only a key:value object. 
-function extractFrontData(txt) {
-  var fd = {};
-  var fMatch = txt
-    .match(/^-{3,}\s*[\r\n]+([^\0]*)[\r\n]+-{3,}[\r\n]*/);
+"use strict";
 
-  if (fMatch) {
-    var fText = fMatch[1].replace(/[\r\n]+/g, "\n");
-
-    // reformat multiline values
-    fText = fText.replaceAll(/>\s*(\n  [^\n]+)+(?=\n*$|\n\S)/g, m => {
-      return (m.substring(1).replace(/\n\s*/g, ' '));
-    });
-
-    var matches = fText.matchAll(/^(?<key>\S+)\s*:\s*(.*)$/gm);
-    for (const match of matches) {
-      fd[match[1]] = match[2];
-    }
-    return ({
-      length: fMatch[0].length,
-      data: fd
-    });
-
-  } else {
-    return ({ length: 0, data: {} });
-  }
-} // extractFrontData
+// poly.js: some minimal polyfills
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+// End.
 
 // lazy load any htm content
 function lazyLoadHTM(container, url) {
